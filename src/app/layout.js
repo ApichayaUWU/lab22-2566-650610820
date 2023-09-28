a2419f43e66881855ceea38d02d4c278629c18f6
FAH <apichaya.inkhiewsai@gmail.com>
2023-09-28 15:31:13 +0700

    fin

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="650610820"
+                fullName="Apichaya Inkhiewsai"
                 year="2023"
               />
             </Container>
